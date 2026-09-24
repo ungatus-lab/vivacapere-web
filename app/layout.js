81dfb54f57d1a2d4d2c2775ebb68431321e1f2b4
ungatus-lab <ungatus@gmail.com>
2026-09-24 12:12:43 +0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "VIVACAPERE OÜ",
+  description: "Official Vivacapere website",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0 }}>{children}</body>
+    </html>
+  );
+}
